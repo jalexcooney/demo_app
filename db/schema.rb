aa3deecc40ee6c48f8aaad8ebe25a39b74603922
Alex Cooney <alexcooney@gmail.com>
2013-06-01 07:14:25 -0500

    mac updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130601050954) do
+ActiveRecord::Schema.define(:version => 20130601120902) do
+
+  create_table "microposts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
